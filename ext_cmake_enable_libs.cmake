93fcd6768d154f875a8ea2dae61b7e930734ba23
Jan Christoph Uhde <Jan@UhdeJC.com>
2020-09-13 20:07:46 +0200

    fix bulild location.

--- ext_cmake_enable_libs.cmake
+++ ext_cmake_enable_libs.cmake
@@ -1,12 +1,13 @@
 macro(ext_enable_ext_libs ext_lib_name)
     set(EXT_LIBRARIES_PATH "${CMAKE_CURRENT_SOURCE_DIR}/..")
-    ##### find ${ext_lib_name} lib #########################################################
+
     if(NOT TARGET ext::${ext_lib_name})
         set(${ext_lib_name}_dir "${EXT_LIBRARIES_PATH}/${ext_lib_name}")
-        ext_log("Searching for ${exit_lib_name} in ${EXT_LIBRARIES_PATH}/${ext_lib_name}")
-        if(EXISTS ${${ext_lib_name}_dir}/include/ext/config.hpp)
+        ext_log("Searching for ${ext_lib_name} in ${EXT_LIBRARIES_PATH}/${ext_lib_name}")
+
+        if(EXISTS ${${ext_lib_name}_dir}/include/ext)
             message(STATUS "extINFO -- found ${ext_lib_name} lib in ${${ext_lib_name}_dir}")
-            add_subdirectory(${${ext_lib_name}_dir} ${${ext_lib_name}_dir}-build EXCLUDE_FROM_ALL)
+            add_subdirectory(${${ext_lib_name}_dir} "${CMAKE_BINARY_DIR}/${ext_lib_name}-build" EXCLUDE_FROM_ALL)
             if(NOT TARGET ext::${ext_lib_name})
                 message(FATAL_ERROR "Adding ${ext_lib_name} lib as add_subdirectory failed.")
             endif()
@@ -26,5 +27,5 @@ macro(ext_enable_ext_libs ext_lib_name)
     if(NOT TARGET ext::${ext_lib_name})
         message(FATAL_ERROR "Could not make ext::${ext_lib_name} library available.")
     endif() # NOT TARGET ext::${ext_lib_name}
-    ##### find ${ext_lib_name} lib #########################################################
+
 endmacro(ext_enable_ext_libs)
